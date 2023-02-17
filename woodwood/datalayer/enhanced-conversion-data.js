9ce14e9cde3622d3036937e7ae09a770c52e107e
markpineecom <117979485+markpineecom@users.noreply.github.com>
2023-02-17 09:31:10 +0100

    Create enhanced-conversion-data.js

---
+++ woodwood/datalayer/enhanced-conversion-data.js
@@ -0,0 +1,15 @@
+<script type="text/javascript">
+  var enhanced_conversion_data = {
+    "first_name": "",
+    "last_name": "",
+    "home_address": {
+      "street": "",
+      "city":"",
+      "region": "",
+      "postal_code": "",
+      "country": "",
+      "email": "",
+      "phone_number": ""
+    }
+  }
+</script>
